6a9b966dcf16fb183379cf65cb6230d860ddae86
i0x46 <i0x45@proton.me>
2024-01-01 00:26:46 +0500

    connect with mongodb atls

---
+++ src/db/index.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async ()=>{
+    try {
+   const connectioninstance =   await  mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+   console.log(`\n MongoDB connected !! DB HOST: ${connectioninstance.connection.host}`)
+    } catch (error) {
+        console.log("MongoDB connection Failld:",error);
+        process.exit(1)
+        
+    }
+}
+export default connectDB
+/**import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("MONGODB connection FAILED ", error);
+        process.exit(1)
+    }
+}
+
+export default connectDB */
