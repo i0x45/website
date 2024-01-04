4f7244214ba617ade3b3be17f4f0acd3d0379be2
i0x46 <i0x45@proton.me>
2024-01-04 22:50:24 +0500

    dependencies

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
+import app from "./app.js"
 dotenv.config({
   path:'./env'
 })
 
- connectDB();
+ connectDB()
+ .then(()=>{
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`server is running a port : ${process.env.PORT}`);
+  })
+ })
+ .catch((err)=>{
+console.log("mongoo db connection fail !!", err);
+ })
